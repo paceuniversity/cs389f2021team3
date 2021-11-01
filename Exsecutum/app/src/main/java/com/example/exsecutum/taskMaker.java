6433fa376b40af1c9087518bd9998f905f48a5b4
cassandra-thomas1 <cassandrart114@gmail.com>
2021-11-01 17:39:51 -0400

    adding the task maker activity

---
+++ Exsecutum/app/src/main/java/com/example/exsecutum/taskMaker.java
@@ -0,0 +1,13 @@
+package com.example.exsecutum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class taskMaker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_maker);
+    }
+}
