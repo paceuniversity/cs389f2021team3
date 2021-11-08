49c6f9975d83057ce5521bd471f634df9ed9ef0e
unknown <rz09697p@pace.edu>
2021-11-08 18:03:34 -0500

    Adding database for tasks.

--- Exsecutum/app/src/main/java/com/example/exsecutum/mySQLiteDBHandler.java
+++ Exsecutum/app/src/main/java/com/example/exsecutum/mySQLiteDBHandler.java
@@ -1,2 +1,35 @@
-package com.example.exsecutum;public class mySQLiteDBHandler {
+/*
+Program Name: mySQLiteDBHandler
+Programmer Names: Robert S. Zecchini
+Version: 1.0
+Purpose: This segment of code creates, assigns, and manages user's tasks by storing the user's
+        tasks within a SQL database.
+*/
+
+package com.example.exsecutum;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class mySQLiteDBHandler extends SQLiteOpenHelper {
+
+    //Constructor class.
+    public mySQLiteDBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //This function overrides the creation of the database.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    //This function overrides how the database gets updated.
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
