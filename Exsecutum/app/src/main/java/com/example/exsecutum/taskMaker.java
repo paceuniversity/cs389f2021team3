27f9867b62cf2743ba294b9477e3849c01efac89
cassandra-thomas1 <cassandrart114@gmail.com>
2021-11-09 20:24:09 -0500

    connecting taskMaker to jump to main activity

--- Exsecutum/app/src/main/java/com/example/exsecutum/taskMaker.java
+++ Exsecutum/app/src/main/java/com/example/exsecutum/taskMaker.java
@@ -1,7 +1,12 @@
 package com.example.exsecutum;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /*
 this class is meant to collect data from the users interaction with the UI
@@ -10,10 +15,29 @@ to be displayed on the calendar.
  */
 
 public class taskMaker extends AppCompatActivity {
+    EditText taskName;
+    Button createTask;
+    public static final String TASK_NAME = "com.example.exsecutum.TASK_NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_maker);
+        taskName = (EditText)findViewById(R.id.editTextTaskName);
+        createTask = (Button)findViewById(R.id.button_CreateTask);
+
+        createTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendTask();
+            }
+        });
+    }
+
+    public void sendTask() {
+        Intent mainPage = new Intent(this, MainActivity.class);
+        String name = taskName.getText().toString();
+        mainPage.putExtra(TASK_NAME, name);
+        startActivity(mainPage);
     }
 }
