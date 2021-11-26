6a6ff593921c30853ea87757b886b3c03fbc3d9b
cassandra-thomas1 <cassandrart114@gmail.com>
2021-11-25 22:21:15 -0500

    tasks are displayed by priority

--- Exsecutum/app/src/main/java/com/example/exsecutum/taskViewAdapter.java
+++ Exsecutum/app/src/main/java/com/example/exsecutum/taskViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.exsecutum;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +11,29 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+
+
 //the purpose of this class is to extend recyclerview so I can actually use it with the tasks
 public class taskViewAdapter extends RecyclerView.Adapter<taskViewAdapter.taskHolder>{
 
     private final ArrayList<Task> tasks;
     Context context;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public taskViewAdapter(Context context, ArrayList<Task> tasks){
         this.context = context;
-        this.tasks = tasks;
+        tasks.sort((t1, t2) -> {        //'compareTo' method did not want to work so Im manually comparing .-.
+            if(t1.getPriority() < t2.getPriority())
+                return 1;
+            else //eventually will need to compare by due date
+                return -1;
+        });
+        this.tasks = tasks;     //tasks is sorted by priority now
+
     }
     @NonNull
     @Override
